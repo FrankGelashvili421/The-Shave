0a9c4d033eea26b19392bbeafa4b7a3433736211
Frank <163575560+FrankGelashvili421@users.noreply.github.com>
2024-09-30 17:04:31 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { BarberShopMainPage } from './BarberShop/MainPage/Main';
+
+export const App= () => {
+
+  return (
+    <div className="App">
+        <BarberShopMainPage/>
+    </div>
+  );
+}
